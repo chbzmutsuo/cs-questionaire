90231da79c6eea2cb604eada3795c11dd6b533ad
chbzmutsuo <411.mutsuo@gmail.com>
2022-05-28 18:16:17 +0900

    tailwind conifgの変更

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,9 @@
 module.exports = {
-  mode: 'jit',
-  purge: ['./src/pages/**/*.{js,ts,jsx,tsx}', './src/components/**/*.{js,ts,jsx,tsx}'],
-  darkMode: false, // or 'media' or 'class'
-  theme: {
-    extend: {},
-  },
-  variants: {
-    extend: {},
-  },
-  plugins: [],
+	mode: 'jit',
+	content: ['./src/pages/**/*.{js,ts,jsx,tsx}', './src/components/**/*.{js,ts,jsx,tsx}'],
+	theme: {
+		extend: {},
+	},
+
+	plugins: [],
 };
